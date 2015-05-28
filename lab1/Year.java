6198a1eef1613537c74e7919fe9589144bb7e3a1
William Tsay <willtsay@gmail.com>
2015-05-27 18:07:26 -0700

    lab1 finish

--- lab1/Year.java
+++ lab1/Year.java
@@ -8,7 +8,10 @@ public class Year {
      *          false if year is not a leap year
      */
     static boolean isLeapYear(int year) {
-        return true;    // YOUR CODE HERE
+        if (year%4 == 0 && year%100 != 0 || year%400==0) {
+            return true;
+        }
+        return false;    // YOUR CODE HERE
     }
 
     /** Calls isLeapYear to print correct statement.
